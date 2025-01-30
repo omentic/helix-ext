97b19934271d2a213860dc5097f8c8342fc9fa4c
JJ <git@toki.la>
2025-01-29 17:10:05 -0800

    Revert "Delay auto-save until exiting insert mode (#11047)"
    This reverts commit dca952c03ac1d3beab1e4b203648e3a31181dbf1.

--- helix-view/src/handlers.rs
+++ helix-view/src/handlers.rs
@@ -8,17 +8,11 @@ pub mod dap;
 pub mod diagnostics;
 pub mod lsp;
 
-#[derive(Debug)]
-pub enum AutoSaveEvent {
-    DocumentChanged { save_after: u64 },
-    LeftInsertMode,
-}
-
 pub struct Handlers {
     // only public because most of the actual implementation is in helix-term right now :/
     pub completions: Sender<lsp::CompletionEvent>,
     pub signature_hints: Sender<lsp::SignatureHelpEvent>,
-    pub auto_save: Sender<AutoSaveEvent>,
+    pub auto_save: Sender<u64>,
 }
 
 impl Handlers {
